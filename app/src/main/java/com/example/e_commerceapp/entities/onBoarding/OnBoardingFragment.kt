d53132dc4cd50b08e0c4b12ccc0405d82944ad29
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-08-14 13:27:13 +0200

    On Boarding Screen

--- app/src/main/java/com/example/e_commerceapp/entities/onBoarding/OnBoardingFragment.kt
+++ app/src/main/java/com/example/e_commerceapp/entities/onBoarding/OnBoardingFragment.kt
@@ -1,22 +1,27 @@
 package com.example.e_commerceapp.entities.onBoarding
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.e_commerceapp.R
+import com.example.e_commerceapp.databinding.FragmentOnBoardingBinding
+import com.example.e_commerceapp.databinding.FragmentSplashBinding
 
 
 class OnBoardingFragment : Fragment() {
-
+    private lateinit var binding: FragmentOnBoardingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+    ): View {
+        binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
 
