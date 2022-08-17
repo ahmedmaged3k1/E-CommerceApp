e9b546d941fcaa34f7e10b983af3f90905bf74f7
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-08-17 10:47:43 +0200

    Details fragment data binding

--- app/src/main/java/com/example/e_commerceapp/ui/features/coffeeDetails/CoffeeDetailsViewModel.kt
+++ app/src/main/java/com/example/e_commerceapp/ui/features/coffeeDetails/CoffeeDetailsViewModel.kt
@@ -13,36 +13,34 @@ class CoffeeDetailsViewModel : ViewModel() {
     val smallSugarPrice = 3
     val mediumSugarPrice = 5
     val largeSugarPrice = 7
-
-    val selectedSugar1: Int = R.drawable.ic_sugar_svgrepo_com
-    val selectedSugar2: Int = R.drawable.ic_sugar_svgrepo_com__1_
-    val selectedSugar3: Int = R.drawable.ic_sugar_svgrepo_com__2_
-    val selectedSize: Int = R.drawable.ic_cup_svgrepo_com
-
-
+    var selectedSugar1 = MutableLiveData<Int>(R.drawable.ic_sugar_svgrepo_com)
+    val selectedSugar2 = MutableLiveData<Int>(R.drawable.ic_sugar_svgrepo_com__1_)
+    val selectedSugar3 = MutableLiveData<Int>(R.drawable.ic_sugar_svgrepo_com__2_)
+    var selectedSize1 = MutableLiveData<Int>(R.drawable.ic_cup_svgrepo_com)
+    val selectedSize2 = MutableLiveData<Int>(R.drawable.ic_cup_svgrepo_com)
+    val selectedSize3 = MutableLiveData<Int>(R.drawable.ic_cup_svgrepo_com)
     var coffeeCounter = MutableLiveData<Int>(0)
     var coffeePrice = MutableLiveData<Int>(0)
-
-
     var coffeeItem = Coffee()
     fun increaseCounter() {
         if (coffeeCounter.value == 0) {
-            coffeePrice.value=0
+            coffeePrice.value = 0
         }
         coffeeCounter.value = coffeeCounter.value?.inc()
-        coffeePrice.value=((coffeeCounter.value?.let { coffeePrice.value?.plus( coffeeItem.price!!) }))
+        coffeePrice.value =
+            ((coffeeCounter.value?.let { coffeePrice.value?.plus(coffeeItem.price!!) }))
 
     }
 
     fun decreaseCounter() {
         if (coffeeCounter.value == 0) {
-            coffeePrice.value=0
+            coffeePrice.value = 0
             coffeeCounter.value = 0
             return
         }
         coffeeCounter.value = coffeeCounter.value?.dec()
-        coffeePrice.value=((coffeeCounter.value?.let { coffeePrice.value?.minus((coffeeItem.price!!)) }))
-
+        coffeePrice.value =
+            ((coffeeCounter.value?.let { coffeePrice.value?.minus((coffeeItem.price!!)) }))
 
 
     }
@@ -50,5 +48,41 @@ class CoffeeDetailsViewModel : ViewModel() {
     fun onClick() {
 
 
+    }
+
+    fun onClickSugar1() {
+        selectedSugar1.value = R.drawable.sugar_hover_1
+        selectedSugar2.value = R.drawable.ic_sugar_svgrepo_com__1_
+        selectedSugar3.value = R.drawable.ic_sugar_svgrepo_com__2_
+    }
+
+    fun onClickSugar2() {
+        selectedSugar2.value = R.drawable.sugar_hover_2
+        selectedSugar1.value = R.drawable.ic_sugar_svgrepo_com
+        selectedSugar3.value = R.drawable.ic_sugar_svgrepo_com__2_
+    }
+
+    fun onClickSugar3() {
+        selectedSugar3.value = R.drawable.sugar_hover_3
+        selectedSugar1.value = R.drawable.ic_sugar_svgrepo_com
+        selectedSugar2.value = R.drawable.ic_sugar_svgrepo_com__1_
+    }
+    fun onClickSize1() {
+        selectedSize1.value = R.drawable.ic_cup_svgrepo_com_hover
+        selectedSize2.value = R.drawable.ic_cup_svgrepo_com
+        selectedSize3.value = R.drawable.ic_cup_svgrepo_com
+
+    }
+
+    fun onClickSize2() {
+        selectedSize2.value = R.drawable.ic_cup_svgrepo_com_hover
+        selectedSize1.value = R.drawable.ic_cup_svgrepo_com
+        selectedSize3.value = R.drawable.ic_cup_svgrepo_com
+    }
+
+    fun onClickSize3() {
+        selectedSize3.value = R.drawable.ic_cup_svgrepo_com_hover
+        selectedSize2.value = R.drawable.ic_cup_svgrepo_com
+        selectedSize1.value = R.drawable.ic_cup_svgrepo_com
     }
 }
